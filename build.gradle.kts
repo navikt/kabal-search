82677a750232cea8820538778755b909589fa78a
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-17 14:23:25 +0100

    New kodeverk

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     implementation("no.nav.security:token-validation-spring:$tokenValidationVersion")
     implementation("no.nav.security:token-client-spring:$tokenValidationVersion")
 
-    implementation("com.github.navikt:kabal-kodeverk:2021.12.14-15.39.5f6715f05a9d")
+    implementation("com.github.navikt:kabal-kodeverk:2021.12.17-13.45.e1c29a4e3a2e")
     implementation("no.nav.slackposter:simple-slack-poster:5")
     implementation("org.springframework.retry:spring-retry:$springRetryVersion")
     implementation("no.finn.unleash:unleash-client-java:$unleashVersion")
