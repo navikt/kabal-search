fe2cd6a629d8c5c40fcec55bbceb64fb5a616bee
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2023-04-11 10:28:55 +0200

    Remove unused Axsys, use public ingresses for norg2 and ereg.

--- src/main/kotlin/no/nav/klage/search/gateway/AxsysGateway.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.klage.search.gateway
-
-import no.nav.klage.search.domain.saksbehandler.Enhet
-
-interface AxsysGateway {
-
-    fun getEnheterForSaksbehandler(ident: String): List<Enhet>
-}
