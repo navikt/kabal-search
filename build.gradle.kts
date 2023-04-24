38a9ba303c3835d3d5835ee035929c9c357c53a8
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2023-04-24 11:18:24 +0200

    Updated kodeverk.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val threeTenExtraVersion = "1.7.2"
 val archunitVersion = "1.0.1"
 val opensearchVersion = "2.6.0"
 val reactorSpringVersion = "1.0.1.RELEASE"
-val kodeverkVersion = "1.2.21"
+val kodeverkVersion = "1.2.22"
 val ehcacheVersion = "3.10.8"
 
 java.sourceCompatibility = JavaVersion.VERSION_17
