65e90234019c7a5e12c4f62887eacbd2fe52deca
Øyvind Wedøe <oyvind.norsted.wedoe@nav.no>
2023-08-29 15:14:32 +0200

    Securelog MSGraph token for debugging.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- src/main/kotlin/no/nav/klage/search/api/controller/SaksbehandlerController.kt
+++ src/main/kotlin/no/nav/klage/search/api/controller/SaksbehandlerController.kt
@@ -11,6 +11,7 @@ import no.nav.klage.search.service.ElasticsearchService
 import no.nav.klage.search.service.saksbehandler.InnloggetSaksbehandlerService
 import no.nav.klage.search.service.saksbehandler.OAuthTokenService
 import no.nav.klage.search.service.saksbehandler.SaksbehandlerService
+import no.nav.klage.search.util.TokenUtil
 import no.nav.klage.search.util.getLogger
 import no.nav.klage.search.util.getSecureLogger
 import no.nav.security.token.support.core.api.ProtectedWithClaims
@@ -26,6 +27,7 @@ class SaksbehandlerController(
     private val oAuthTokenService: OAuthTokenService,
     private val innloggetSaksbehandlerService: InnloggetSaksbehandlerService,
     private val saksbehandlerService: SaksbehandlerService,
+    private val tokenUtil: TokenUtil,
 ) {
 
     companion object {
@@ -70,6 +72,7 @@ class SaksbehandlerController(
         val saksbehandlereFromMSGraph = saksbehandlerService.getSaksbehandlereForEnhet(enhetsnummer = enhet)
 
         secureLogger.debug("fromMS: {}", saksbehandlereFromMSGraph)
+        secureLogger.debug("MSGraph app token: {}", tokenUtil.getAppAccessTokenWithGraphScope())
 
         return SaksbehandlereListResponse(
             saksbehandlere = (saksbehandlereFromES + saksbehandlereFromMSGraph)
