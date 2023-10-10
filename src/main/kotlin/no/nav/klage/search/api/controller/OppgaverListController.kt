455597b976cabeacf0108f8ed0fe5448b7536e7c
Øyvind Wedøe <oyvind.norsted.wedoe@nav.no>
2023-10-10 10:18:38 +0200

    Removed duplicate endpoints.

--- src/main/kotlin/no/nav/klage/search/api/controller/OppgaverListController.kt
+++ src/main/kotlin/no/nav/klage/search/api/controller/OppgaverListController.kt
@@ -44,29 +44,6 @@ class OppgaverListController(
     }
 
     @Operation(
-        summary = "Hent ledige ROL-oppgaver",
-        description = "Henter alle ledige ROL-oppgaver."
-    )
-    @GetMapping("/roloppgaver/ledige", produces = ["application/json"])
-    fun getLedigeRolOppgaver(
-        queryParams: MineLedigeOppgaverQueryParams
-    ): BehandlingerListResponse {
-        logger.debug("Params: {}", queryParams)
-
-        val searchCriteria = behandlingerSearchCriteriaMapper.toLedigeOppgaverSearchCriteria(
-            queryParams = queryParams,
-        )
-
-        val esResponse = elasticsearchService.findLedigeROLOppgaverByCriteria(searchCriteria)
-        return BehandlingerListResponse(
-            antallTreffTotalt = esResponse.totalHits.toInt(),
-            behandlinger = behandlingListMapper.mapEsBehandlingerToListView(
-                esBehandlinger = esResponse.searchHits.map { it.content },
-            )
-        )
-    }
-
-    @Operation(
         summary = "Hent oppgave",
         description = "Hent oppgave."
     )
@@ -113,54 +90,6 @@ class OppgaverListController(
     }
 
     @Operation(
-        summary = "Hent ferdigstilte ROL-oppgaver for en ansatt",
-        description = "Henter alle ferdigstilte ROL-oppgaver som ROL har tilgang til."
-    )
-    @GetMapping("/roloppgaver/ferdigstilte", produces = ["application/json"])
-    fun getMineFerdigstilteROLOppgaver(
-        queryParams: MineFerdigstilteOppgaverQueryParams
-    ): BehandlingerListResponse {
-        logger.debug("Params: {}", queryParams)
-
-        val searchCriteria = behandlingerSearchCriteriaMapper.toFerdigstilteOppgaverSearchCriteria(
-            navIdent = oAuthTokenService.getInnloggetIdent(),
-            queryParams = queryParams
-        )
-
-        val esResponse = elasticsearchService.findROLsFerdigstilteOppgaverByCriteria(searchCriteria)
-        return BehandlingerListResponse(
-            antallTreffTotalt = esResponse.totalHits.toInt(),
-            behandlinger = behandlingListMapper.mapEsBehandlingerToListView(
-                esBehandlinger = esResponse.searchHits.map { it.content },
-            ),
-        )
-    }
-
-    @Operation(
-        summary = "Hent returnert ROL-oppgaver for en ansatt",
-        description = "Henter alle returnerte ROL-oppgaver som ROL har tilgang til."
-    )
-    @GetMapping("/roloppgaver/returnerte", produces = ["application/json"])
-    fun getMineReturnerteROLOppgaver(
-        queryParams: MineReturnerteROLOppgaverQueryParams
-    ): BehandlingerListResponse {
-        logger.debug("Params: {}", queryParams)
-
-        val searchCriteria = behandlingerSearchCriteriaMapper.toReturnerteROLOppgaverSearchCriteria(
-            navIdent = oAuthTokenService.getInnloggetIdent(),
-            queryParams = queryParams
-        )
-
-        val esResponse = elasticsearchService.findROLsReturnerteOppgaverByCriteria(searchCriteria)
-        return BehandlingerListResponse(
-            antallTreffTotalt = esResponse.totalHits.toInt(),
-            behandlinger = behandlingListMapper.mapEsBehandlingerToListView(
-                esBehandlinger = esResponse.searchHits.map { it.content },
-            ),
-        )
-    }
-
-    @Operation(
         summary = "Hent uferdige oppgaver for en ansatt",
         description = "Henter alle uferdige oppgaver som saksbehandler har tilgang til."
     )
@@ -176,30 +105,6 @@ class OppgaverListController(
         )
 
         val esResponse = elasticsearchService.findSaksbehandlersUferdigeOppgaverByCriteria(searchCriteria)
-        return BehandlingerListResponse(
-            antallTreffTotalt = esResponse.totalHits.toInt(),
-            behandlinger = behandlingListMapper.mapEsBehandlingerToListView(
-                esBehandlinger = esResponse.searchHits.map { it.content },
-            ),
-        )
-    }
-
-    @Operation(
-        summary = "Hent uferdige ROL-oppgaver for en ansatt",
-        description = "Henter alle uferdige ROL-oppgaver som ROL har tilgang til."
-    )
-    @GetMapping("/roloppgaver/uferdige", produces = ["application/json"])
-    fun getMineUferdigeROLOppgaver(
-        queryParams: MineUferdigeOppgaverQueryParams
-    ): BehandlingerListResponse {
-        logger.debug("Params: {}", queryParams)
-
-        val searchCriteria = behandlingerSearchCriteriaMapper.toUferdigeOppgaverSearchCriteria(
-            navIdent = oAuthTokenService.getInnloggetIdent(),
-            queryParams = queryParams
-        )
-
-        val esResponse = elasticsearchService.findROLsUferdigeOppgaverByCriteria(searchCriteria)
         return BehandlingerListResponse(
             antallTreffTotalt = esResponse.totalHits.toInt(),
             behandlinger = behandlingListMapper.mapEsBehandlingerToListView(
