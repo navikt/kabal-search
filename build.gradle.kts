ea4c48ac4b59eb8421b10559bfcd7a4b6a62d275
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-01-31 14:35:07 +0100

    Updated kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val threeTenExtraVersion = "1.7.2"
 val archunitVersion = "1.2.1"
 val opensearchVersion = "2.11.1"
 val reactorSpringVersion = "1.0.1.RELEASE"
-val kodeverkVersion = "1.7.25"
+val kodeverkVersion = "1.7.28"
 val ehcacheVersion = "3.10.8"
 val logbackSyslog4jVersion = "1.0.0"
 
