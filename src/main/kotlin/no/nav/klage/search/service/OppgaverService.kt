9cd788488757c2c46158ac252471ec87bf508f7f
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-09-16 11:37:58 +0200

    Include new type in ledige oppgaver.

--- src/main/kotlin/no/nav/klage/search/service/OppgaverService.kt
+++ src/main/kotlin/no/nav/klage/search/service/OppgaverService.kt
@@ -34,7 +34,7 @@ class OppgaverService(
         )
 
         val typer = queryParams.typer.ifEmpty {
-            listOf(Type.KLAGE.id, Type.ANKE.id)
+            listOf(Type.KLAGE.id, Type.ANKE.id, Type.BEHANDLING_ETTER_TRYGDERETTEN_OPPHEVET.id)
         }
 
         val searchCriteria = behandlingerSearchCriteriaMapper.toLedigeOppgaverSearchCriteria(
@@ -70,7 +70,7 @@ class OppgaverService(
         )
 
         val typer = queryParams.typer.ifEmpty {
-            listOf(Type.KLAGE.id, Type.ANKE.id)
+            listOf(Type.KLAGE.id, Type.ANKE.id, Type.BEHANDLING_ETTER_TRYGDERETTEN_OPPHEVET.id)
         }
 
         val searchCriteria = behandlingerSearchCriteriaMapper.toSearchCriteriaForLedigeMedUtgaattFrist(
