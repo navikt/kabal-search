b30079c1122434260ff7e78a7c57a6356641d67e
Andreas Jonsson <andreas.jonsson@nav.no>
2025-02-24 16:29:49 +0100

    New kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val threeTenExtraVersion = "1.8.0"
 val archunitVersion = "1.4.0"
 val opensearchVersion = "2.19.0"
 val reactorSpringVersion = "1.0.1.RELEASE"
-val kodeverkVersion = "1.9.18"
+val kodeverkVersion = "1.9.20"
 val ehcacheVersion = "3.10.8"
 val logbackSyslog4jVersion = "1.0.0"
 
