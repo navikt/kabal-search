2b8f664c1e4886cd1cb9bcedef0c88cc2eec2755
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-07-21 14:10:11 +0200

    Try different setup.

--- src/main/kotlin/no/nav/klage/search/clients/egenansatt/EgenAnsattKafkaConsumer.kt
+++ src/main/kotlin/no/nav/klage/search/clients/egenansatt/EgenAnsattKafkaConsumer.kt
@@ -45,17 +45,17 @@ class EgenAnsattKafkaConsumer(
 //            partitionOffsets = [PartitionOffset(partition = "*", initialOffset = "0")]
 //        )]
 //    )
-//    fun listen(egenAnsattRecord: ConsumerRecord<String, String>) {
-//        runCatching {
-//            logger.debug("Reading offset ${egenAnsattRecord.offset()} from partition ${egenAnsattRecord.partition()} on kafka topic ${egenAnsattRecord.topic()}")
-//            val foedselsnr = egenAnsattRecord.key()
-//            val egenAnsatt = egenAnsattRecord.value().toEgenAnsatt()
-//            egenAnsattService.oppdaterEgenAnsatt(foedselsnr, egenAnsatt)
-//        }.onFailure {
-//            teamLogger.error("Failed to process egenansatt record", it)
-//            throw RuntimeException("Could not process egenansatt record. See more details in team-logs.")
-//        }
-//    }
+    fun listen(egenAnsattRecord: ConsumerRecord<String, String>) {
+        runCatching {
+            logger.debug("Reading offset ${egenAnsattRecord.offset()} from partition ${egenAnsattRecord.partition()} on kafka topic ${egenAnsattRecord.topic()}")
+            val foedselsnr = egenAnsattRecord.key()
+            val egenAnsatt = egenAnsattRecord.value().toEgenAnsatt()
+            egenAnsattService.oppdaterEgenAnsatt(foedselsnr, egenAnsatt)
+        }.onFailure {
+            teamLogger.error("Failed to process egenansatt record", it)
+            throw RuntimeException("Could not process egenansatt record. See more details in team-logs.")
+        }
+    }
 
     private fun String.toEgenAnsatt() = mapper.readValue(this, EgenAnsatt::class.java)
 }
