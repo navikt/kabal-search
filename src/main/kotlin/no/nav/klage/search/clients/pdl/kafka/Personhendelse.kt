cf0984c6e1368a3e6c9923cb5a6959de381a1c54
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-07-21 14:27:45 +0200

    Try different setup.

--- src/main/kotlin/no/nav/klage/search/clients/pdl/kafka/Personhendelse.kt
+++ src/main/kotlin/no/nav/klage/search/clients/pdl/kafka/Personhendelse.kt
@@ -5,3 +5,9 @@ import org.apache.avro.generic.GenericRecord
 val GenericRecord.opplysningstype get(): String {
     return get("opplysningstype").toString()
 }
+
+val GenericRecord.erAdressebeskyttelse get(): Boolean {
+    return opplysningstype == OPPLYSNINGSTYPE_ADRESSEBESKYTTELSE
+}
+
+const val OPPLYSNINGSTYPE_ADRESSEBESKYTTELSE = "ADRESSEBESKYTTELSE_V1"
